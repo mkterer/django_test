fe9799c366580eb3966ff0e77b468abb56139a5c
mkterer <>
2021-06-09 08:01:06 +0900

    ormの練習

--- project_name/blog/views.py
+++ project_name/blog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Post
+from django.contrib.auth.models import User
 
 # Create your views here.
 def posts(request):
@@ -11,4 +13,16 @@ def advans(request):
     return render(request, 'blog/advans.html', {})
 
 def gets(request):
+    print(Post.objects.all()) # >> post all
+    print(User.objects.all()) # >> username all
+    print(User.objects.get(username='test')) # >> test
+    
+    post = Post.objects.get(id='1')
+    print(post.content) # >> 22222222
+    post.content = 'test update'
+    post.save() # update
+    print(post.content) # >> test update
+    
+    delete_post = Post.objects.get(id='3')
+    delete_post.delete() # >> records with id 3 are deleted
     return render(request, 'blog/gets.html', {})
